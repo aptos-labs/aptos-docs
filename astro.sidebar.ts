bce50efa919a9579dc5a0f7ef2edea343f05b99e
astinz <28899947+astinz@users.noreply.github.com>
2026-03-04 02:48:08 +0300

    lint & test

--- astro.sidebar.ts
+++ astro.sidebar.ts
@@ -247,9 +247,7 @@ export const sidebar = [
                   "build/sdks/community-sdks/kotlin-sdk/client-configuration",
                   group("build.group.sdks.community-sdks.kotlin-sdk.fetch-data", {
                     collapsed: true,
-                    items: [
-                      "build/sdks/community-sdks/kotlin-sdk/fetch-data/fetch-data-via-sdk",
-                    ],
+                    items: ["build/sdks/community-sdks/kotlin-sdk/fetch-data/fetch-data-via-sdk"],
                   }),
                   "build/sdks/community-sdks/kotlin-sdk/sponsored-transactions",
                   group("build.group.sdks.community-sdks.kotlin-sdk.for-ios-devs", {
